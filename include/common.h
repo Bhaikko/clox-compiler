deb88dc45e6745275985454820fe6ff864ae4c04
Siddharth <siddhartha.pawar17@gmail.com>
2022-03-08 14:24:01 +0530

    chunk file added

---
+++ include/common.h
@@ -0,0 +1,11 @@
+// File used to include header files commonly used in other files
+// To import standard data types
+
+#ifndef clox_common_h
+#define clox_common_h 
+
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
