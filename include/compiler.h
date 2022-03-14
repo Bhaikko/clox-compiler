b98cc7aebba2dd9a6829c6f474eef0339bad9ecb
Siddharth <siddhartha.pawar17@gmail.com>
2022-03-14 23:05:05 +0530

    Parsing methods for precedence parsing added

--- include/compiler.h
+++ include/compiler.h
@@ -12,6 +12,28 @@ typedef struct {
     bool panicMode;     // For synchronizing in case of syntax errors
 } Parser;
 
+/*
+ This exists because Praser does not cascade to parse expressions
+ of higher precedence. Hence a difference solution is implemented where
+ parsePrecedence(precedence) will parse any expression at the given
+ precedence level or higher.
+
+ Lower enums have higher precedence
+*/
+typedef enum {
+    PREC_NONE,
+    PREC_ASSIGNMENT,    // =
+    PREC_OR,            // or
+    PREC_AND,           // and
+    PREC_EQUALITY,      // == !=
+    PREC_COMPARISON,    // < > <= >=
+    PREC_TERM,          // + -
+    PREC_FACTOR,        // * /
+    PREC_UNARY,         // * /
+    PREC_CALL,          // . ()
+    PREC_PRIMARY        
+} Precedence;
+
 bool compile(const char* source, Chunk* chunk);
 
 #endif
--- lib/compiler.c
+++ lib/compiler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "./../include/common.h"
 #include "./../include/compiler.h"
@@ -6,6 +7,8 @@
 
 Parser parser;
 Chunk* compilingChunk;
+
+// CODE GENERATION FUNCTIONS
 
 // Writes the given byte to chunk
 static void emitByte(uint8_t byte)
@@ -20,9 +23,27 @@ static void emitBytes(uint8_t byte1, uint8_t byte2)
     emitByte(byte2);
 }
 
-static emitReturn()
+static void emitReturn()
 {
     emitByte(OP_RETURN);
+}
+
+static uint8_t makeConstant(Value value)
+{
+    int constant = addConstant(currentChunk(), value);
+
+    // Restricting max number of constants in chunk
+    if (constant > UINT8_MAX) {
+        error("Too many constants in one chunk.");
+        return 0;
+    }
+
+    return (uint8_t)constant;
+}
+
+static void emitConstant(Value value)
+{
+    emitBytes(OP_CONSTANT, makeConstant(value));
 }
 
 static Chunk* currentChunk()
@@ -36,6 +57,7 @@ static void endCompiler()
     emitReturn();
 }
 
+// PARSER UTILITIES
 static void errorAt(Token* token, const char* message)
 {
     if (parser.panicMode) {
@@ -98,6 +120,55 @@ static void consume(TokenType type, const char* message)
 
     errorAtCurrent(message);
 }
+
+// PARSING FUNCTIONS
+
+// Parses any expression at the given precedence level or higher
+static void parsePrecedence(Precedence precedence)
+{
+
+}
+
+static void expression()
+{
+    parsePrecedence(PREC_ASSIGNMENT);
+}
+
+// Parsing number literal consisting of single token
+static void number()
+{
+    double value = strtod(parser.previous.start, NULL);
+    emitConstant(value);
+}
+
+// Parsing grouping expressions
+static void grouping()
+{
+    // '(' is assumed to be consumed earlier before calling grouping()
+
+    // This takes care of generating bytecode for expression inside the parenthesis
+    expression();   
+    consume(TOKEN_RIGHT_PAREN, "Expect ')' after expression.");
+}
+
+static void unary()
+{
+    TokenType operatorType = parser.previous.type;
+
+    // Since operand is evaluated first which pushes the value to stack
+    // Then its negation is done
+    // Compile the operand
+    parsePrecedence(PREC_UNARY);
+
+    // Emiting the operator instruction
+    switch (operatorType) {
+        case TOKEN_MINUS: emitByte(OP_NEGATE); break;
+
+        default:
+            return;
+    }
+}
+
 
 bool compile(const char* source, Chunk* chunk)
 {
